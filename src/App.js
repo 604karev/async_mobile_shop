e527dc3a60ed4313ac0ed9786c4450ea0fd8d912
604karev <604karev@gmail.com>
2018-12-06 12:52:51 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {createStore, applyMiddleware} from 'redux';
+import reducers from './reducers';
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import {BrowserRouter} from 'react-router-dom';
+
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <BrowserRouter>
+                <Provider store={store}>
+                    <div className="App">
+                    </div>
+                </Provider>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
