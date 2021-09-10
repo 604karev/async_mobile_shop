d6d13b51306cf9f20dcbd021abd31e35812e26b8
604karev <604karev@gmail.com>
2021-09-10 15:01:12 +0300

    refactor: change components paths

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './node_modules/index.css';
+import App from './node_modules/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "babel-polyfill";
 import "isomorphic-fetch";
@@ -12,4 +11,4 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
